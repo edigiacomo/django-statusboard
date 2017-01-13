b56284412784bd2ed2c2114c014a04ced110ea85
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2017-01-13 15:57:11 +0100

    HyperlinkedModelSerializer

--- statuspage/serializers.py
+++ statuspage/serializers.py
@@ -3,14 +3,24 @@ from rest_framework import serializers
 from .models import Service, ServiceGroup
 
 
-class ServiceSerializer(serializers.ModelSerializer):
+class ServiceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Service
         fields = ('id', 'name', 'description', 'href', 'status', 'groups',
                   'created', 'modified')
+        extra_kwargs = {
+            'groups': {
+                'view_name': 'statuspage:api:servicegroup-detail',
+            }
+        }
 
 
-class ServiceGroupSerializer(serializers.ModelSerializer):
+class ServiceGroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ServiceGroup
         fields = ('id', 'name', 'services', 'created', 'modified')
+        extra_kwargs = {
+            'services': {
+                'view_name': 'statuspage:api:service-detail',
+            }
+        }
