af08b541559495b7ae4d723ec331d9d7200a7ef8
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2017-01-19 10:57:30 +0100

    Removed hyperlink in API

--- statuspage/serializers.py
+++ statuspage/serializers.py
@@ -3,35 +3,20 @@ from rest_framework import serializers
 from .models import Service, ServiceGroup, Incident
 
 
-class ServiceSerializer(serializers.HyperlinkedModelSerializer):
+class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = ('id', 'name', 'description', 'href', 'status', 'groups',
                   'created', 'modified')
-        extra_kwargs = {
-            'groups': {
-                'view_name': 'statuspage:api:servicegroup-detail',
-            }
-        }
 
 
-class ServiceGroupSerializer(serializers.HyperlinkedModelSerializer):
+class ServiceGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceGroup
         fields = ('id', 'name', 'services', 'created', 'modified')
-        extra_kwargs = {
-            'services': {
-                'view_name': 'statuspage:api:service-detail',
-            }
-        }
 
 
-class IncidentSerializer(serializers.HyperlinkedModelSerializer):
+class IncidentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Incident
         fields = ('id', 'name', 'service', 'status', 'description', 'occurred')
-        extra_kwargs = {
-            'service': {
-                'view_name': 'statuspage:api:service-detail',
-            }
-        }
