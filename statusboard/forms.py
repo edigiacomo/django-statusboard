26bfdc24ab2f9bd180053ad5b3f5a2e6137ec675
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2017-02-07 07:40:14 +0100

    service status in incident form

--- statusboard/forms.py
+++ statusboard/forms.py
@@ -2,9 +2,15 @@ from django import forms
 
 from .models import Incident
 from .models import Service
+from .models import SERVICE_STATUSES
 
 
 class IncidentForm(forms.ModelForm):
+
+    service_status = forms.IntegerField(
+        label='service_status', widget=forms.Select(choices=SERVICE_STATUSES),
+    )
+
     class Meta:
         model = Incident
-        fields = ['name', 'service', 'occurred']
+        fields = ['name', 'service', 'occurred', 'service_status']
