b0f8636ae9b13ff4ed30f4dcdd28a36f661d1a68
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2017-02-13 10:57:08 +0100

    statusboard.utils.PermissionRequiredMixin

---
+++ statusboard/utils.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.contrib.auth.views import redirect_to_login
+
+
+class PermissionRequiredMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        if self.permission_required:
+            if not self.request.user.has_perm(self.permission_required):
+                return redirect_to_login(self.request.get_full_path())
+
+        return super(PermissionRequiredMixin, self).dispatch(request, *args, **kwargs)
