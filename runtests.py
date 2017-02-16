aef62bed425207338dcbc2fda2f792105e6ca04e
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2017-02-16 08:43:38 +0100

    runtest args

--- runtests.py
+++ runtests.py
@@ -6,13 +6,17 @@ from django.conf import settings
 from django.test.utils import get_runner
 
 
-def runtests():
+def runtests(test_labels=["tests"]):
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(["tests"])
+    failures = test_runner.run_tests(test_labels)
     sys.exit(bool(failures))
 
 if __name__ == '__main__':
-    runtests()
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("test_label", nargs="*", default=["tests"])
+    args = parser.parse_args()
+    runtests(args.test_label)
