03ee6614f673239dbe3916b0214a04de5aed1655
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2021-05-21 07:24:44 +0200

    Use pypandoc.convert_file

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_version(package):
 def read_md(path):
     try:
         import pypandoc
-        return pypandoc.convert(path, 'rst')
+        return pypandoc.convert_file(path, 'rst')
     except ImportError:
         return open(path).read()
 
