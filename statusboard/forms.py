88a69b9a442d79dd45050af8e17032bfbf43dd9e
Emanuele Di Giacomo <edigiacomo@arpae.it>
2022-11-19 16:40:50 +0100

    Minor fixes to translation

--- statusboard/forms.py
+++ statusboard/forms.py
@@ -56,12 +56,7 @@ class IncidentForm(forms.ModelForm):
     service_status = forms.ChoiceField(
         choices=tuple([("", "---------")] + list(SERVICE_STATUSES)),
         label=_("Services status"),
-        help_text=_(
-            (
-                "Update the status of involved services "
-                "(an empty value means that they will be left unaltered)"
-            )
-        ),
+        help_text=_("Update the status of involved services (an empty value means that they will be left unaltered)"),
         required=False,
     )
 
