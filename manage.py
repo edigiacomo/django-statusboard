149c697f36f4c4a0989490959467919c00016863
Emanuele Di Giacomo <edigiacomo@arpae.it>
2022-11-19 16:39:10 +0100

    Use test settings in manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
-import sys
+import os
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
+import django
+from django.core.management import execute_from_command_line
+
 
 if __name__ == "__main__":
-    import django
-    from django.core.management import execute_from_command_line
     django.setup()
-    execute_from_command_line(sys.argv)
+    execute_from_command_line()
